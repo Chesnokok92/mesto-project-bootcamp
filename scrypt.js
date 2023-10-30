7eba67631d5fda313c6642ee38986537659cf2e2
Chesnokok92 <chesnokok@gmail.com>
2023-10-30 12:56:59 +0300

    1
    На проверку

---
+++ scrypt.js
@@ -0,0 +1,4 @@
+let elements = document.querySelector('.elements');
+let elements__like = document.querySelector('.elements__like');
+
+let elements__like = container.querySelector('.elements__element__like_active ');
